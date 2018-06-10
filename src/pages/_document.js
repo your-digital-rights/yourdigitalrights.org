af0370d15f1ab278d6ce83c93646565123753239
Rob Chandler <robkadt@gmail.com>
2018-06-10 16:24:49 +0100

    Add GTM

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -63,6 +63,12 @@ export default class IntlDocument extends Document {
     return (
       <html>
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html:
+                "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M9LPCR2');"
+            }}
+          />
           <title>Opt-out</title>
           <meta name="description" content={Description} />
           <meta property="og:description" content={OgDescription} />
@@ -90,6 +96,12 @@ export default class IntlDocument extends Document {
           <link rel="icon" href="/static/favicon.ico" />
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html:
+                '<iframe src=“https://www.googletagmanager.com/ns.html?id=GTM-M9LPCR2” height=“0" width=“0” style=“display:none;visibility:hidden”></iframe>'
+            }}
+          />
           <Main />
           <script src={polyfill} />
           <script
