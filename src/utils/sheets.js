bda068afd9e8b605a1bd21c18234cfdbae079b72
Rob Chandler <robkadt@gmail.com>
2018-06-11 22:31:39 +0100

    Fetch companies data from opt-out-api

--- src/utils/sheets.js
+++ src/utils/sheets.js
@@ -1,16 +1,6 @@
 import fetch from "universal-fetch";
-const SHEET_ID = "1tBtKWcOnLOs2cwqs_EX0ldTCaG3gh_7neQpaIYHBvJE";
 export default async function fetchData() {
-  let data = await fetch(
-    `https://spreadsheets.google.com/feeds/list/${SHEET_ID}/od6/public/values?alt=json`
-  );
+  let data = await fetch(`https://opt-out-api.now.sh/companies`);
   data = await data.json();
-  data = data.feed.entry.map(company => {
-    return {
-      name: company["gsx$companyname"]["$t"],
-      email: company["gsx$email"]["$t"],
-      url: company["gsx$companyurl"]["$t"]
-    };
-  });
   return data;
 }
