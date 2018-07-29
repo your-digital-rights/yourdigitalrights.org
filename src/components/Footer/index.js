e1d8048dcc1413767231bb002ad0ba4ab49428fc
Rafa <mprada.rafael@gmail.com>
2018-07-29 12:32:50 +0100

    wishlist link and improved footer
    - added a button to take users to the wishlist
    - change to 2 columns footer with policy and about as hyperlinks
    - expanded the whole body to max-width 1000px

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,4 @@
+import Button from "@material-ui/core/Button";
 import { FormattedMessage } from "react-intl";
 import Typography from "@material-ui/core/Typography";
 import { container } from "../../styles/layout";
@@ -8,21 +9,102 @@ const styles = theme => ({
     ...container
   },
   inner: {
-    borderTop: "2px solid #005ea5",
+    // borderTop: "2px solid #005ea5",
     marginTop: 30,
     paddingTop: 30,
     paddingBottom: 30,
     [theme.breakpoints.up("md")]: {
-      paddingRight: 90,
-      paddingLeft: 90
+      paddingRight: 30,
+      paddingLeft: 30
+    }
+  },
+  innerLeft: {
+    // borderTop: "2px solid #005ea5",
+    width: "20%",
+    float: "left",
+    marginBottom: 30,
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+    }
+  },
+  innerRight: {
+    // borderTop: "2px solid #005ea5",
+    width: "80%",
+    float: "right",
+    marginBottom: 60,
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+    }
+  },
+  WishList: {
+    textAlign: "center",
+  },
+  wishButton: {
+    marginTop: "-25px",
+    borderRadius:"24px 24px 24px 24px",
+    color: "white",
+    fontWeight: "600",
+    padding: '10px 20px',
+    '&:hover': {
+      background: '#cf8600'
+    }
+  },
+  DisclaimerLink: {
+    color: "#f2f2f2",
+    fontWeight: "600",
+    textDecoration: "none",
+    '&:hover': {
+      textDecoration: "underline",
     }
   }
 });
 
+const WishButtonText = (
+  <FormattedMessage
+    id="WishList"
+    defaultMessage="make a wish on our roadmap"
+  />
+);
+
+const PrivacyLink = (
+  <FormattedMessage
+    id="Privacy"
+    defaultMessage="{ToPrivacy}"
+    values={{
+      ToPrivacy: (
+        <a href="/privacy" style={{ textDecoration: "none", fontWeight: "600"}}>
+          Privacy Policy
+        </a>
+      )
+    }}
+  />
+);
+
+const MissionLink = (
+  <FormattedMessage
+    id="Mission"
+    defaultMessage="{ToMission}"
+    values={{
+      ToMission: (
+        <a href="/about" style={{ textDecoration: "none", fontWeight: "600"}}>
+          Mission
+        </a>
+      )
+    }}
+  />
+);
+
 const DisclaimerText = (
   <FormattedMessage
     id="disclaimer"
-    defaultMessage="This service is provided as is, without warranty of any kind. Use of this service is entirely at your own risk. We cannot take responsibility for any direct or indirect damages resulting from the use of this service. The information provided by this service along with the content on our website related to legal matters is provided for your private use and does not constitute legal advice. If you need legal advice for a specific problem, you should consult with a licensed attorney."
+    defaultMessage="{title} This service is provided as is, without warranty of any kind. Use of this service is entirely at your own risk. We cannot take responsibility for any direct or indirect damages resulting from the use of this service. The information provided by this service along with the content on our website related to legal matters is provided for your private use and does not constitute legal advice. If you need legal advice for a specific problem, you should consult with a licensed attorney."
+    values={{
+      title: (
+        <span style={{ fontWeight: "bold", color: "black" }}>
+          Disclaimer:
+        </span>
+      ),
+    }}
   />
 );
 
@@ -32,28 +114,43 @@ const QueryText = (
     defaultMessage="For more information, please read our {privacy} or contact us at {mail}."
     values={{
       privacy: (
-        <a href="/privacy" style={{ fontWeight: "bold" }}>
+        <a href="/privacy" style={{ textDecoration: "none", color: "black" }}>
           Privacy Policy
         </a>
       ),
       mail: (
-        <a href="mailto:info@opt-out.eu" style={{ fontWeight: "bold" }}>
+        <a href="mailto:info@opt-out.eu" style={{ textDecoration: "none" }}>
           info@opt-out.eu
         </a>
       )
     }}
-  /> 
+  />
 );
 
 
 const Footer = ({ classes }) => {
   return (
     <div className={classes.root}>
+      <div className={classes.WishList}>
+        <Button variant="raised" href="https://wishlist.opt-out.eu/" color="secondary" type="submit" className={classes.wishButton}>
+          {WishButtonText}
+        </Button>
+      </div>
       <div className={classes.inner}>
-        <Typography gutterBottom={true} color="textSecondary">
-          {DisclaimerText}
-        </Typography>
-        <Typography color="textSecondary">{QueryText}</Typography>
+        <div className={classes.innerLeft}>
+          <Typography gutterBottom={true} className={classes.DisclaimerLink}>
+            {PrivacyLink}
+          </Typography>
+          <Typography gutterBottom={true} className={classes.DisclaimerLink}>
+            {MissionLink}
+          </Typography>
+        </div>
+        <div className={classes.innerRight}>
+          <Typography gutterBottom={true} color="textSecondary">
+            {DisclaimerText}
+          </Typography>
+          <Typography color="textSecondary">{QueryText}</Typography>
+        </div>
       </div>
     </div>
   );
