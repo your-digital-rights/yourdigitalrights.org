b1a1534edeaf19ecfff80bc4c85eff35fb885fb2
Yoav Aviram <yoav@cleverblocks.com>
2019-04-04 16:18:57 +0200

    Sitemap and robots working

--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
 // Polyfill Node with `Intl` that has data for all locales.
 // See: https://formatjs.io/guides/runtime-environments/#server
-const IntlPolyfill = require("intl");
+const IntlPolyfill = require("react-intl");
 Intl.NumberFormat = IntlPolyfill.NumberFormat;
 Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat;
 
 const { readFileSync } = require("fs");
 const { basename } = require("path");
-const { createServer } = require("http");
+const express = require("express");
 const accepts = require("accepts");
 const glob = require("glob");
 const next = require("next");
+const sitemapAndRobots = require('./scripts/sitemapAndRobots')
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const app = next({ dir: './src', dev });
 const handle = app.getRequestHandler();
+
 
 // Get the supported languages by looking for translations in the `lang/` dir.
 const languages = glob.sync("./lang/*.json").map(f => basename(f, ".json"));
@@ -40,14 +42,21 @@ const getMessages = locale => {
 };
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const accept = accepts(req);
-    const locale = accept.language(dev ? ["en"] : languages);
-    req.locale = locale;
-    req.localeDataScript = getLocaleDataScript(locale);
-    req.messages = dev ? {} : getMessages(locale);
-    handle(req, res);
-  }).listen(port, err => {
+  const server = express();
+
+  sitemapAndRobots({ server });
+  
+  server.get('*', (req, res) => {
+      const accept = accepts(req);
+      const locale = accept.language(dev ? ["en"] : languages);
+      req.locale = locale;
+      req.localeDataScript = getLocaleDataScript(locale);
+      req.messages = dev ? {} : getMessages(locale);
+      handle(req, res)
+
+  });
+  
+  server.listen(port, err => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
