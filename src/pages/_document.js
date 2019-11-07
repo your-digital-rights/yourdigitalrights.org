9b54809c5e8b516bdb988c5c15738c327a1156f5
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2019-11-07 10:52:56 +0100

    Bug fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -61,7 +61,7 @@ export default class IntlDocument extends Document {
         <Head>
           <meta
             property="og:image"
-            content="https://" + DOMAIN + "/static/opt-out-share.jpg?v=2"
+            content={"https://" + DOMAIN + "/static/opt-out-share.jpg?v=2"}
           />
           <meta
             property="og:image:width"
@@ -82,7 +82,7 @@ export default class IntlDocument extends Document {
 
           <meta
             name="twitter:image"
-            content="https://" + DOMAIN + "/static/opt-out-share.jpg?v=2"
+            content={"https://" + DOMAIN + "/static/opt-out-share.jpg?v=2"}
           />
           <meta
             name="viewport"
@@ -107,7 +107,7 @@ export default class IntlDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html:
-                "var _paq = _paq || [];_paq.push(['setDocumentTitle', document.domain + '/' + document.title]); _paq.push(['setCookieDomain', '*." + DOMAIN + "']); _paq.push(['setDomains', ['*." DOMAIN + "']]); _paq.push(['trackPageView']); _paq.push(['enableLinkTracking']); (function() { var u='https://optout.innocraft.cloud/'; _paq.push(['setTrackerUrl', u+'piwik.php']); _paq.push(['setSiteId', '1']); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s); })();"
+                "var _paq = _paq || [];_paq.push(['setDocumentTitle', document.domain + '/' + document.title]); _paq.push(['setCookieDomain', '*." + DOMAIN + "']); _paq.push(['setDomains', ['*." + DOMAIN + "']]); _paq.push(['trackPageView']); _paq.push(['enableLinkTracking']); (function() { var u='https://optout.innocraft.cloud/'; _paq.push(['setTrackerUrl', u+'piwik.php']); _paq.push(['setSiteId', '1']); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s); })();"
             }}
           />
           <noscript>
