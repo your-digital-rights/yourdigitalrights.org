d23b31593643bf7519a83d5a264b88bf9b68e3ad
steevevadakkumchery <steeve.g.v@gmail.com>
2019-11-09 15:21:40 +0000

    removed prettier from HowItWorks

--- src/components/HowItWorks/UpperSection.js
+++ src/components/HowItWorks/UpperSection.js
@@ -1,23 +1,23 @@
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
 const styles = theme => ({
   root: {
-    marginBottom: 16,
+    marginBottom: 16
   },
   heading: {
-    display: 'inline',
-    color: '#005ea5',
-    fontWeight: '600',
+    display: "inline",
+    color: "#005ea5",
+    fontWeight: "600"
   },
   inline: {
-    display: 'inline',
-  },
+    display: "inline"
+  }
 });
 const UpperSection = ({ title, body, classes }) => {
   return (
     <div className={classes.root}>
       <Typography component="h3" className={classes.heading} variant="title">
-        {title}.{' '}
+        {title}.{" "}
       </Typography>
       <Typography component="p" className={classes.inline}>
         {body}
--- src/components/HowItWorks/styles.js
+++ src/components/HowItWorks/styles.js
@@ -1,9 +1,9 @@
-import { container } from '../../styles/layout';
+import { container } from "../../styles/layout";
 export const indexStyles = theme => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    paddingTop: '30px',
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: "30px",
     ...container,
     [theme.breakpoints.down('sm')]: {
       marginTop: '-100px',
@@ -11,34 +11,34 @@ export const indexStyles = theme => ({
     },
   },
   title: {
-    textAlign: 'center',
-    [theme.breakpoints.up('md')]: {
-      textAlign: 'left',
-    },
+    textAlign: "center",
+    [theme.breakpoints.up("md")]: {
+      textAlign: "left"
+    }
   },
   titleImg: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'block',
-    },
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "block"
+    }
   },
   upperContainer: {
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-      alignItems: 'center',
-      margin: '60px 0',
-    },
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+      alignItems: "center",
+      margin: "60px 0"
+    }
   },
   upperContent: {
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up("md")]: {
       flex: 1,
-      marginLeft: 64,
-    },
+      marginLeft: 64
+    }
   },
   lowerContainer: {
-    [theme.breakpoints.up('sm')]: {
-      display: 'flex',
-      alignItems: 'top',
-    },
-  },
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+      alignItems: "top"
+    }
+  }
 });
