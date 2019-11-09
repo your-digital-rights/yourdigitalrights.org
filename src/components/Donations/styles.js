b1be1f46b061674d7b8aff523051287eabb271b7
steevevadakkumchery <steeve.g.v@gmail.com>
2019-11-09 15:00:20 +0000

    removed prettier changes

--- src/components/Donations/styles.js
+++ src/components/Donations/styles.js
@@ -1,56 +1,56 @@
-import { container } from '../../styles/layout';
-import { themeBg } from '../../styles/theme';
+import { container } from "../../styles/layout";
+import { themeBg } from "../../styles/theme";
 
 export default theme => ({
   donate: {
-    backgroundColor: '#f2f2f2',
+    backgroundColor: "#f2f2f2",
   },
   heading: {
-    maxWidth: '850px !important',
-    margin: 'auto auto',
-
+    maxWidth: "850px !important",
+    margin: "auto auto",
+    
     [theme.breakpoints.down('sm')]: {
       marginTop: '-140px',
       paddingTop: '150px',
     },
   },
   container: {
-    padding: '30px',
-    boxSizing: 'border-box',
+    padding: "30px",
+    boxSizing: "border-box",
     ...container,
-    [theme.breakpoints.up('md')]: {
-      padding: '76px 30px',
+    [theme.breakpoints.up("md")]: {
+      padding: "76px 30px",
       backgroundImage: "url('static/QR.svg')",
-      backgroundPosition: 'right 130px top 120px',
-      backgroundRepeat: 'no-repeat',
-    },
+      backgroundPosition: "right 130px top 120px",
+      backgroundRepeat: "no-repeat"
+    }
   },
   donateButton: {
-    marginTop: '-25px',
-    borderRadius: '24px 24px 24px 24px',
-    color: 'white',
-    fontWeight: '600',
+    marginTop: "-25px",
+    borderRadius:"24px 24px 24px 24px",
+    color: "white",
+    fontWeight: "600",
     padding: '10px 20px',
     '&:hover': {
-      background: '#04487B',
+      background: '#04487B'
     },
   },
   intro: {
-    marginBottom: '50px',
-    maxWidth: '530px',
+    marginBottom: "50px",
+    maxWidth: "530px"
   },
   titleImg: {
-    width: '300px',
-    maxWidth: '75%',
+    width: "300px",
+    maxWidth: "75%"
   },
   title: {
-    marginBottom: 0,
+    marginBottom: 0
   },
   bitcoin: {
     textAlign: 'center',
-    marginBottom: '30px',
-    [theme.breakpoints.up('sm')]: {
+    marginBottom: "30px",
+    [theme.breakpoints.up("sm")]: {
       textAlign: 'left',
-    },
-  },
+    }
+  }
 });
