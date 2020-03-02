8076f44380d026e5dc0a030d4cb213808df74bfb
Rafa Prada <mprada.rafael@gmail.com>
2020-03-02 00:55:12 +0000

    Changes on donate

--- src/components/Donations/styles.js
+++ src/components/Donations/styles.js
@@ -3,16 +3,16 @@ import { themeBg } from "../../styles/theme";
 
 export default theme => ({
   donate: {
-    backgroundColor: "#f2f2f2",
+    backgroundColor: "#f2f2f2"
   },
   heading: {
     maxWidth: "850px !important",
     margin: "auto auto",
-    
-    [theme.breakpoints.down('sm')]: {
-      marginTop: '-140px',
-      paddingTop: '150px',
-    },
+
+    [theme.breakpoints.down("sm")]: {
+      marginTop: "-140px",
+      paddingTop: "150px"
+    }
   },
   container: {
     padding: "30px",
@@ -27,17 +27,17 @@ export default theme => ({
   },
   donateButton: {
     marginTop: "-25px",
-    borderRadius:"24px 24px 24px 24px",
+    borderRadius: "24px 24px 24px 24px",
     color: "white",
     fontWeight: "600",
-    padding: '10px 20px',
-    '&:hover': {
-      background: '#04487B'
-    },
+    padding: "10px 20px",
+    "&:hover": {
+      background: "#04487B"
+    }
   },
   intro: {
     marginBottom: "50px",
-    maxWidth: "530px"
+    maxWidth: "570px"
   },
   titleImg: {
     width: "300px",
@@ -47,10 +47,10 @@ export default theme => ({
     marginBottom: 0
   },
   bitcoin: {
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: "30px",
     [theme.breakpoints.up("sm")]: {
-      textAlign: 'left',
+      textAlign: "left"
     }
   }
 });
