0a89b9447bb009ca602789efc54ceddea4856105
Rafa Prada <mprada.rafael@gmail.com>
2020-03-03 10:08:03 +0000

    menu CTA fix

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,198 +1,197 @@
-import { FormattedMessage } from 'react-intl';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import React, { Component } from 'react';
-import tracking from '../../utils/tracking';
-import classNames from 'classnames';
+import { FormattedMessage } from "react-intl";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import React, { Component } from "react";
+import tracking from "../../utils/tracking";
+import classNames from "classnames";
 
 const styles = theme => ({
   nav: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: '0 60px',
-    backgroundColor: '#005ea5',
-    borderBottom: '4px solid #0a74be',
-    height: '72px',
-    width: '100%',
-    zIndex: '11000',
-    [theme.breakpoints.down('xs')]: {
-      padding: '0 15px',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "0 60px",
+    backgroundColor: "#005ea5",
+    borderBottom: "4px solid #0a74be",
+    height: "72px",
+    width: "100%",
+    zIndex: "11000",
+    [theme.breakpoints.down("xs")]: {
+      padding: "0 15px"
     },
-    position: 'fixed',
-    top: '0',
+    position: "fixed",
+    top: "0"
   },
   logoLink: {
-    display: 'flex',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center"
   },
   logo: {
-    width: '90px',
-    outlineColor: '#e8f4f8',
+    width: "90px",
+    outlineColor: "#e8f4f8",
 
-    [theme.breakpoints.down('xs')]: {
-      width: '80px',
-    },
+    [theme.breakpoints.down("xs")]: {
+      width: "80px"
+    }
   },
   container: {
-    listStyle: 'none',
-    display: 'flex',
-    [theme.breakpoints.down('sm')]: {
-      display: 'none',
-    },
+    listStyle: "none",
+    display: "flex",
+    [theme.breakpoints.down("sm")]: {
+      display: "none"
+    }
   },
   item: {
-    padding: '10px 0',
-    marginRight: '24px',
-    outlineColor: '#e8f4f8',
+    padding: "10px 0",
+    marginRight: "24px",
+    outlineColor: "#e8f4f8"
   },
   link: {
-    color: '#f6f7fa',
-    fontWeight: 'bolder',
-    fontSize: '15px',
-    textDecoration: 'none',
-    outlineColor: '#e8f4f8',
+    color: "#f6f7fa",
+    fontWeight: "bolder",
+    fontSize: "15px",
+    textDecoration: "none",
+    outlineColor: "#e8f4f8"
   },
   subsectionLink: {
-    color: '#bebebe',
-    fontWeight: 'bolder',
-    fontSize: '15px',
-    textDecoration: 'none',
-    outlineColor: '#e8f4f8',
+    color: "#bebebe",
+    fontWeight: "bolder",
+    fontSize: "15px",
+    textDecoration: "none",
+    outlineColor: "#e8f4f8"
   },
   hamburgerButton: {
-    display: 'none',
-    height: '50px',
-    cursor: 'pointer',
-    outlineColor: '#e8f4f8',
+    display: "none",
+    height: "50px",
+    cursor: "pointer",
+    outlineColor: "#e8f4f8",
 
-    [theme.breakpoints.down('sm')]: {
-      display: 'block',
-    },
+    [theme.breakpoints.down("sm")]: {
+      display: "block"
+    }
   },
   navChildren: {
-    position: 'fixed',
-    height: '50px',
-    width: '100%',
-    top: '70px',
-    zIndex: '9999',
+    position: "fixed",
+    height: "50px",
+    width: "100%",
+    top: "70px",
+    zIndex: "9999"
   },
   mobileListContainer: {
-    display: 'none',
-    width: '250px',
-    height: '110vh',
-    position: 'fixed',
-    right: '0',
-    top: '0',
-    float: 'right',
-    color: '#ffffff',
-    overflowX: 'hidden',
-    overflowY: 'hidden',
-    zIndex: '10000',
+    display: "none",
+    width: "250px",
+    height: "110vh",
+    position: "fixed",
+    right: "0",
+    top: "0",
+    float: "right",
+    color: "#ffffff",
+    overflowX: "hidden",
+    overflowY: "hidden",
+    zIndex: "10000",
 
-    [theme.breakpoints.down('sm')]: {
-      display: 'flex',
-      justifyContent: 'center',
-    },
+    [theme.breakpoints.down("sm")]: {
+      display: "flex",
+      justifyContent: "center"
+    }
   },
   hideMobContainer: {
     width: 0
   },
   showMobContainer: {
-    width: '250px'
+    width: "250px"
   },
   scrollOut: {
-    position: 'absolute',
-    right: '-252px',
-    transition: ' right 0.5s',
+    position: "absolute",
+    right: "-252px",
+    transition: " right 0.5s"
   },
   scrollIn: {
-    position: 'absolute',
-    right: '0',
-    transition: 'right 0.5s',
+    position: "absolute",
+    right: "0",
+    transition: "right 0.5s"
   },
   mobileList: {
-    display: 'flex',
-    listStyle: 'none',
-    alignItems: 'flex-start',
-    flexDirection: 'column',
-    backgroundColor: '#005ea5',
-    position: 'absolute',
-    right: '0',
-    top: '0',
-    margin: '0',
-    width: '252px',
-    height: '110vh',
-    padding: '100px 25px',
-    zIndex: '11000',
+    display: "flex",
+    listStyle: "none",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    backgroundColor: "#005ea5",
+    position: "absolute",
+    right: "0",
+    top: "0",
+    margin: "0",
+    width: "252px",
+    height: "110vh",
+    padding: "100px 25px",
+    zIndex: "11000"
   },
   OptOutRedButton: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 'auto',
-    height: '40px',
-    backgroundColor: '#ef6a6e',
-    fontSize: '14px',
-    fontWeight: '800',
-    fontStyle: 'normal',
-    fontStretch: 'normal',
-    lineHeight: '1.15',
-    letterSpacing: '1.25px',
-    color: '#ffffff',
-    borderRadius: '25px',
-    margin: '25px 0',
-    outlineColor: '#e8f4f8',
-    cursor: 'pointer',
-    paddingLeft: '20px',
-    paddingRight: '20px',
-    textDecoration: 'none',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "auto",
+    height: "40px",
+    backgroundColor: "#ef6a6e",
+    fontSize: "14px",
+    fontWeight: "400",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "1.15",
+    letterSpacing: "0.25px",
+    color: "#ffffff",
+    borderRadius: "25px",
+    margin: "25px 0",
+    outlineColor: "#e8f4f8",
+    cursor: "pointer",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    textDecoration: "none"
   },
   OptOutRedButtonDesktop: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 'auto',
-    height: '40px',
-    backgroundColor: '#ef6a6e',
-    fontSize: '14px',
-    fontWeight: '800',
-    fontStyle: 'normal',
-    fontStretch: 'normal',
-    lineHeight: '1.15',
-    letterSpacing: '1.25px',
-    color: '#ffffff',
-    borderRadius: '25px',
-    outlineColor: '#e8f4f8',
-    cursor: 'pointer',
-    marginRight: '24px',
-    paddingLeft: '20px',
-    paddingRight: '20px',
-    textDecoration: 'none',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "auto",
+    height: "40px",
+    backgroundColor: "#ef6a6e",
+    fontSize: "14px",
+    fontWeight: "800",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "1.15",
+    letterSpacing: "1.25px",
+    color: "#ffffff",
+    borderRadius: "25px",
+    outlineColor: "#e8f4f8",
+    cursor: "pointer",
+    marginRight: "24px",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    textDecoration: "none"
   },
   fadeBackground: {
-    position: 'fixed',
-    top: '0',
-    width: '100vw',
-    height: '100vh',
-    backgroundColor: 'rgba(255,255,255, 0.5)',
-    zIndex: '9999',
-    display: 'none',
+    position: "fixed",
+    top: "0",
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "rgba(255,255,255, 0.5)",
+    zIndex: "9999",
+    display: "none",
 
-    [theme.breakpoints.down('sm')]: {
-      display: 'block',
-    },
+    [theme.breakpoints.down("sm")]: {
+      display: "block"
+    }
   },
   twitterHandle: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '120px',
-    alignItems: 'center',
-  },
+    display: "flex",
+    justifyContent: "space-between",
+    width: "120px",
+    alignItems: "center"
+  }
 });
 
-
-const trackSearchButtonLinkClick = (device) => {
+const trackSearchButtonLinkClick = device => {
   tracking.trackSearchButtonLinkClick(device);
 };
 
@@ -205,10 +204,7 @@ const NavItem = ({
   target
 }) => {
   return (
-    <li
-      className={classes.item}
-      onClick={onClickHandler}
-    >
+    <li className={classes.item} onClick={onClickHandler}>
       <Typography
         component="a"
         target={target}
@@ -262,7 +258,7 @@ const NavListDesktop = ({ classes }) => {
         className={classes.OptOutRedButtonDesktop}
         tabIndex={0}
         onClick={() => {
-          trackSearchButtonLinkClick('desktop');
+          trackSearchButtonLinkClick("desktop");
         }}
       >
         <Typography component="span" className={classes.link}>
@@ -275,7 +271,12 @@ const NavListDesktop = ({ classes }) => {
 
 const NavListMobile = ({ classes, mobileNavOpen, toggleMobileNav }) => {
   return (
-    <div className={classNames(mobileNavOpen ? classes.scrollIn : classes.scrollOut, 'mob-navbar')}>
+    <div
+      className={classNames(
+        mobileNavOpen ? classes.scrollIn : classes.scrollOut,
+        "mob-navbar"
+      )}
+    >
       <ul className={classes.mobileList}>
         <NavItem
           onClickHandler={toggleMobileNav}
@@ -311,7 +312,7 @@ const NavListMobile = ({ classes, mobileNavOpen, toggleMobileNav }) => {
           tabIndex={0}
           onClick={() => {
             toggleMobileNav();
-            trackSearchButtonLinkClick('mobile');
+            trackSearchButtonLinkClick("mobile");
           }}
         >
           <Typography component="span" className={classes.link}>
@@ -386,7 +387,7 @@ class Nav extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mobileNavOpen: false,
+      mobileNavOpen: false
     };
     this.toggleMenu = React.createRef();
     this.hamburgerButton = React.createRef();
@@ -397,17 +398,17 @@ class Nav extends Component {
   }
 
   componentDidMount() {
-    window.addEventListener('click', this.handleCloseNav);
+    window.addEventListener("click", this.handleCloseNav);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('click', this.handleCloseNav);
+    window.removeEventListener("click", this.handleCloseNav);
   }
 
   toggleMobileNav() {
     this.setState(currentState => {
       return {
-        mobileNavOpen: !currentState.mobileNavOpen,
+        mobileNavOpen: !currentState.mobileNavOpen
       };
     });
   }
@@ -430,19 +431,15 @@ class Nav extends Component {
       <div>
         <nav ref={this.toggleMenu} className={classes.nav}>
           <a className={classes.logoLink} href="/">
-            <img
-              className={classes.logo}
-              src="static/type.svg"
-              tabIndex={0}
-            />
+            <img className={classes.logo} src="static/type.svg" tabIndex={0} />
           </a>
           <NavListDesktop classes={classes} />
           <img
             className={classes.hamburgerButton}
             src={
               mobileNavOpen
-                ? 'static/close-icon.svg'
-                : 'static/hamburgerIcon.svg'
+                ? "static/close-icon.svg"
+                : "static/hamburgerIcon.svg"
             }
             onBlur={this.onBlurHandler}
             onClick={this.toggleMobileNav}
@@ -453,7 +450,10 @@ class Nav extends Component {
 
         <div
           ref={this.hamburgerButton}
-          className={classNames(classes.mobileListContainer, mobileNavOpen ? classes.showMobContainer: classes.hideMobContainer)}
+          className={classNames(
+            classes.mobileListContainer,
+            mobileNavOpen ? classes.showMobContainer : classes.hideMobContainer
+          )}
           onFocus={this.onFocusHandler}
         >
           <NavListMobile
