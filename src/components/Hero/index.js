f52ec715f2352bc0ef0c96914982b3b7348789af
Rafa Prada <mprada.rafael@gmail.com>
2020-05-31 19:45:48 +0100

    Add random prettier formatting

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,5 +1,5 @@
-import { IntroText, SubtitleText, DataBrokers} from "./text";
-import Link from 'next/link'
+import { IntroText, SubtitleText, DataBrokers } from "./text";
+import Link from "next/link";
 
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
@@ -16,18 +16,28 @@ const Hero = ({ classes, onCompanySelected, children }) => {
             <span style={visuallyHidden}>Your Digital Rights</span>
           </h2>
           <Typography
-              variant="display1"
-              color="inherit"
-              gutterBottom={true}
-              component="p"
+            variant="display1"
+            color="inherit"
+            gutterBottom={true}
+            component="p"
           >
             {SubtitleText}
           </Typography>
           <Typography color="inherit" className={classes.intro}>
             {IntroText}
           </Typography>
-          <Typography color="inherit" className={classes.introEnd} component="span">
-            <p>Don't know where to start? Opt out of these <Link className={classes.introLink} href="/data-brokers">Data Brokers</Link>.</p>
+          <Typography
+            color="inherit"
+            className={classes.introEnd}
+            component="span"
+          >
+            <p>
+              Don't know where to start? Opt out of these{" "}
+              <Link className={classes.introLink} href="/data-brokers">
+                Data Brokers
+              </Link>
+              .
+            </p>
           </Typography>
           {children}
         </div>
