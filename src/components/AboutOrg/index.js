23acc46787880e9fb580fe254bec6398a5db3d4e
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2020-06-19 14:42:57 +0200

    Disqus

--- src/components/AboutOrg/index.js
+++ src/components/AboutOrg/index.js
@@ -3,8 +3,15 @@ import Typography from "@material-ui/core/Typography";
 import styles from "./styles";
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import { DiscussionEmbed } from 'disqus-react';
 
 const AboutOrg = ({ classes, selectedCompany, canonical }) => {
+  const threadConfig = {
+    url: canonical,
+    identifier: selectedCompany.url,
+    title: selectedCompany.name,
+  };
+
   return (
     <div className={classes.about}>
       <div id="about-org" className={classes.container}>
@@ -52,41 +59,8 @@ const AboutOrg = ({ classes, selectedCompany, canonical }) => {
           <Typography color="inherit" className={classes.discussionHeading}>
             Do you have something to say about privacy at {selectedCompany.name}?
           </Typography>
-          <div
-            id="discourse-comments"
-            className={classes.discourseComments}
-          ></div>
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html:
-                "DiscourseEmbed = { discourseUrl: 'https://optout.discourse.group/', discourseEmbedUrl: '" +
-                canonical +
-                "'}; (function() {var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true; d.src = DiscourseEmbed.discourseUrl + 'javascripts/embed.js'; (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d); })();",
-            }}
-          />
 
-          <div id="disqus_thread"></div>
-
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html:
-                "var disqus_config = function () { this.page.url = '" + canonical + "' this.page.identifier = '" + selectedCompany.url + "'}; (function() { var d = document, s = d.createElement('script'); s.src = 'https://your-digital-rights.disqus.com/embed.js'; s.setAttribute('data-timestamp', +new Date()); (d.head || d.body).appendChild(s); })();"
-            }}
-          />
-
-          {/* <div>
-            <Button
-              className={classes.Continue}
-              variant="contained"
-              href=""
-              color="primary"
-              type="submit"
-            >
-              Continue the conversation on Discourse
-            </Button>
-          </div> */}
+          <DiscussionEmbed shortname={selectedCompany.url} config={threadConfig} />
         </div>
       </div>
     </div>
