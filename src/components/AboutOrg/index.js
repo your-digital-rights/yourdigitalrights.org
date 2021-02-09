fbd2c1fad94dcb7329ffe41693382abba8c3afc7
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2021-02-09 13:24:59 +0100

    Remarkbox integration

--- src/components/AboutOrg/index.js
+++ src/components/AboutOrg/index.js
@@ -3,9 +3,15 @@ import Typography from "@material-ui/core/Typography";
 import styles from "./styles";
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import { DOMAIN } from "../../utils/domain";
 
 const AboutOrg = ({ classes, selectedCompany, canonical }) => {
-
+  var rb_owner_key = "e91628b9-67ed-11eb-8f71-040140774501";
+  var thread_title = "Privacy at " + selectedCompany.url;
+  var rb_src = "https://my.remarkbox.com/embed" + 
+      "?rb_owner_key=" + rb_owner_key +
+      "&thread_title=" + encodeURI(thread_title) +
+      "&thread_uri=" + encodeURIComponent(canonical);
   return (
     <div className={classes.about}>
       <div id="about-org" className={classes.container}>
@@ -48,6 +54,21 @@ const AboutOrg = ({ classes, selectedCompany, canonical }) => {
             Number of request sent:{" "}
             <strong>{selectedCompany.emailsSent}</strong>
           </div>
+        </div>
+        <div id="discussion" className={classes.discussion}>
+          <Typography color="inherit" className={classes.discussionHeading}>
+            Do you have something to say about privacy at {selectedCompany.name}?
+          </Typography>
+          <div id="remarkbox-div">
+              <iframe id="remarkbox-iframe" src={rb_src} style={{ width: '100%' }} tabindex={0} scrolling="no" frameborder={0} title="Remarkbox"  ></iframe>
+          </div>
+          <script src="https://my.remarkbox.com/static/js/iframe-resizer/iframeResizer.min.js" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html:
+                'var thread_fragment = window.location.hash; iFrameResize( { checkOrigin: ["https://my.remarkbox.com"], inPageLinks: true, initCallback: function(e) {e.iFrameResizer.moveToAnchor(thread_fragment)} }, document.getElementById("remarkbox-iframe"));',
+            }}
+          />
         </div>
       </div>
     </div>
