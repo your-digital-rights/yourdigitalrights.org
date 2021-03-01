4dbf6b8cd86f50b2bfc4ae0d39f21e0c3612af77
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2021-03-01 18:03:13 +0100

    Working, needs styling

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import CookieConsent from "react-cookie-consent";
+import { CookieBanner } from '@palmabit/react-cookie-law';
 
 // Overide custom app to place cookie consent on every page.
 
@@ -14,12 +15,39 @@ export default function App({ Component, pageProps }) {
           />
       	</Head>
 	  	<Component {...pageProps} />
-		<CookieConsent
-		  buttonText="Ok"
-		  buttonStyle={{ color: "#4e503b", fontSize: "13px", borderRadius: "3px" }}
-		>
-		  This website uses cookies to enhance the user experience and for analytics, please see our <a style={{color: "#ffffff"}} href="/privacy">Privacy Policy</a> page for details.
-		</CookieConsent>
+      <div>
+        <CookieBanner
+          message="This website uses cookies to enhance the user experience and for analytics."
+          policyLink="/privacy"
+          privacyPolicyLinkText="Privacy Policy"
+          showPreferencesOption={false}
+          showMarketingOption={false}
+          statisticsDefaultChecked={true}
+          wholeDomain={true}
+          onAcceptStatistics = {() => {_paq.push(['rememberCookieConsentGiven']);}}
+          onAcceptStatistics = {() => {_paq.push(['forgetCookieConsentGiven']);}}
+          styles={{
+            container: {
+              fontFamily: 'Source Sans Pro',
+              height: 57,
+              background: 'rgba(52, 64, 81, 0.88) url(/cookie.png) 20px 100% no-repeat',
+              backgroundSize: '30px 30px',
+              backgroundColor: 'white',
+              fontSize: '15px',
+              fontWeight: 600,
+              position: 'fixed',
+              bottom: '0',
+              left: '0',
+              right: '0',
+              zIndex: '100000',
+              padding: '10px',              
+            },            
+            message: {
+              fontColor: 'white',
+            }
+          }}
+        />
+      </div>
 	</>
   )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -99,7 +99,7 @@ export default class IntlDocument extends Document {
                 DOMAIN +
                 "']); _paq.push(['setDomains', ['*." +
                 DOMAIN +
-                "']]); _paq.push(['trackPageView']); _paq.push(['enableLinkTracking']); (function() { var u='https://optout.innocraft.cloud/'; _paq.push(['setTrackerUrl', u+'matomo.php']); _paq.push(['setSiteId', '2']); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript'; g.async=true; g.defer=true; g.src='//cdn.innocraft.cloud/optout.innocraft.cloud/matomo.js'; s.parentNode.insertBefore(g,s); })();",
+                "']]); _paq.push(['requireCookieConsent']); _paq.push(['trackPageView']); _paq.push(['enableLinkTracking']); (function() { var u='https://optout.innocraft.cloud/'; _paq.push(['setTrackerUrl', u+'matomo.php']); _paq.push(['setSiteId', '2']); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript'; g.async=true; g.defer=true; g.src='//cdn.innocraft.cloud/optout.innocraft.cloud/matomo.js'; s.parentNode.insertBefore(g,s); })();",
             }}
           />
           <noscript>
