10cb0174592e54c8e63ab811a8ea09b51f481a3d
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2021-03-13 14:55:15 +0100

    fetchSheetData: explicitly wait on response.json

--- src/utils/sheets.js
+++ src/utils/sheets.js
@@ -5,13 +5,17 @@ export default async function fetchData() {
   const url = `https://api.yourdigitalrights.org/companies`;
 
   if (data == null) {
-    data = fetch(url).then(function (response) {
-      if (response.status >= 400) {
-        throw new Error(`Bad response from ${url}`);
-      }
+    data = fetch(url)
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error(`Bad response from ${url}`);
+        }
 
-      return response.json();
-    });
+        return response.json();
+      })
+      .then((json) => {
+        return json;
+      });
   }
 
   return data;
