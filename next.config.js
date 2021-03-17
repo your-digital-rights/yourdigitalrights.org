14179e5487e480615d5cc571d6a8989a76fe5f78
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2021-03-17 14:15:28 +0100

    wip

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 module.exports = {
 	target: 'serverless',
 	async rewrites() {
-  		return [
-  			{ source: "/sitemap.xml", destination: "/api/sitemap" },
-  		];
+			return [
+				{ source: "/sitemap.xml", destination: "/api/sitemap" },
+			];
 	},
+	i18n: {
+	// These are all the locales you want to support in
+	// your application
+	locales: ["en", "es"],
+	// This is the default locale you want to be used when visiting
+	// a non-locale prefixed path e.g. `/hello`
+	defaultLocale: "en",
+	},	
 };
--- src/pages/d/[domain].js
+++ src/pages/d/[domain].js
@@ -12,79 +12,81 @@ import pageWithIntl from "../../components/PageWithIntl";
 import tracking from "../../utils/tracking";
 import withRoot from "../../withRoot";
 import { DOMAIN } from "../../utils/domain";
-import Error from "next/error";
 
 
 function Capitalize(str){
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
+const Org = ({ newOrg, organization, classes }) => {
 
-class Org extends Component {
-  constructor(props) {
-    super(props);
+  const Title = organization ? Capitalize(organization.url) + " - Delete Your Account or Get a Copy of Your Data | YourDigitalRight.org" : "Send GDPR and CCPA Data Deletion and Access Requests | YourDigitalRight.org";
+  const Description = organization ? "Request account deletion or a copy of your personal data from  " + Capitalize(organization.url) + " quickly and easily with YourDigitalRight.org - a FREE service which makes exercising your right to privacy easy." :
+    "Send CCPA and GDPR data deletion and access requests to any organization quickly and easily with YourDigitalRight.org - a FREE service which makes exercising your right to privacy easy.";
+  const Canonical = organization ? "https://" + DOMAIN + "/d/" + organization.url : "https://" + DOMAIN + "/d/add";
 
-    this.state = { 
-      ipcountry: "",
-    };
-  }
-
-  static async getInitialProps({ req, query, res }) {
-    this.setState({ ipcountry: req.headers['host'] });
-    if (query.domain) {
-      if (query.domain == 'add') {
-        return { newOrg: true }
-      }
-      const organizations = await fetchSheetData();
-      const organization = organizations.find(
-        ({ url }) => query.domain === url
-      );
-      if ({organization}) {
-        return { organization };
-      }
-    } 
-  }
-
-  render() {
-    const { newOrg, organization, classes } = this.props;
-
-    if (!newOrg && !organization) return <Error statusCode={404} />;
-
-    const Title = organization ? Capitalize(organization.url) + " - Delete Your Account or Get a Copy of Your Data | YourDigitalRight.org" : "Send GDPR and CCPA Data Deletion and Access Requests | YourDigitalRight.org";
-    const Description = organization ? "Request account deletion or a copy of your personal data from  " + Capitalize(organization.url) + " quickly and easily with YourDigitalRight.org - a FREE service which makes exercising your right to privacy easy." :
-      "Send CCPA and GDPR data deletion and access requests to any organization quickly and easily with YourDigitalRight.org - a FREE service which makes exercising your right to privacy easy.";
-    const Canonical = organization ? "https://" + DOMAIN + "/d/" + organization.url : "https://" + DOMAIN + "/d/add";
-
-    return (
-      <div>
-        <Head>
-          <title>{Title}</title>
-          <link rel="canonical" href={Canonical} />
-          <meta name="description" content={Description} />
-          <meta property="og:description" content={Description} />
-          <meta property="og:title" content={Title} />
-          <meta name="twitter:title" content={Title} />
-          <meta name="twitter:description" content={Description} />
-        </Head>
-        <Nav />
-        <Hero 
+  return (
+    <div>
+      <Head>
+        <title>{Title}</title>
+        <link rel="canonical" href={Canonical} />
+        <meta name="description" content={Description} />
+        <meta property="og:description" content={Description} />
+        <meta property="og:title" content={Title} />
+        <meta name="twitter:title" content={Title} />
+        <meta name="twitter:description" content={Description} />
+      </Head>
+      <Nav />
+      <Hero 
+        selectedCompany={organization}
+      />
+      <PersonalInfoForm
+        selectedCompany={organization}
+      />
+      {organization && (
+        <AboutOrg 
           selectedCompany={organization}
+          canonical={Canonical}
         />
-        <PersonalInfoForm
-          selectedCompany={organization}
-          ipcountry={this.state.ipcountry}
-        />
-        {organization && (
-          <AboutOrg 
-            selectedCompany={organization}
-            canonical={Canonical}
-          />
-        )}
-        <Donations />
-        <Footer />
-      </div>
-    );
+      )}
+      <Donations />
+      <Footer />
+    </div>
+  )
+}
+
+export async function getStaticPaths() {
+  const organizations = await fetchSheetData();
+
+  const paths = organizations.map((org) => ({
+    params: { domain: org.url },
+  }))
+  
+  return { paths, fallback: 'blocking' }
+}
+
+export async function getStaticProps({ params }) {
+  if (params.domain == 'add') {
+    return {
+      props: {
+        newOrg: true,   
+      }
+    }
+  }  
+
+  const organizations = await fetchSheetData();
+  const organization = organizations.find(
+    ({ url }) => params.domain === url
+  );
+
+  return {
+    notFound: organization.length == 0,
+    props: {
+      organizations,
+    },
+    revalidate: 60*60
   }
 }
+
 
 export default withRoot(pageWithIntl(Org));
