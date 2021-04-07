ff0f07d5df22e73729700ab9ead93f5080622d0f
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2021-04-07 12:16:36 +0200

    Add call for translations on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -111,12 +111,18 @@ const About = ({ classes }) => {
           <Typography gutterBottom={true}>
             <FormattedMessage
               id="helpUsBody1"
-              defaultMessage="If you are concerned about the loss of privacy, rampant surveillance capitalism, micro-targeting and misinformation and the addictive nature of digital services then we’d love for you to join us! We’re looking for volunteers, particularly React devs and data scientists, but anyone is welcome. Please contact us at {email}."
+              defaultMessage="If you are concerned about the loss of privacy, rampant surveillance capitalism, micro-targeting and misinformation and the addictive nature of digital services then we’d love for you to join us!"
+            />
+          </Typography>
+          <Typography gutterBottom={true}>
+            <FormattedMessage
+              id="helpUsBody2"
+              defaultMessage="We’re looking for volunteers including React developers, and people who can help us translate the website in to additional European languages, but anyone is welcome. Please contact us at {email}."
               values= {{
                 email: <a href="mailto:info@opt-out.eu">info@opt-out.eu</a>,
               }}
             />
-          </Typography>
+          </Typography>          
           <br />          
           <Typography gutterBottom={true} component="h2" variant="h3">
             <FormattedMessage id="funding" defaultMessage="Funding" />
