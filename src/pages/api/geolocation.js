5e0905b22a6aabd1c99435d3670774f6d06faa38
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2021-07-16 11:31:15 +0200

    Change get headers syntax

--- src/pages/api/geolocation.js
+++ src/pages/api/geolocation.js
@@ -3,9 +3,9 @@ export default async (req, res) => {
         res.status(400).json({err: "Geolocation unavaialble."});
     } else {
         res.status(200).json({ 
-            country: req.headers('x-vercel-ip-country'),
-            region: req.headers('x-vercel-ip-country-region'),
-            city: req.headers('x-vercel-ip-city')
+            country: req.headers['x-vercel-ip-country'],
+            region: req.headers['x-vercel-ip-country-region'],
+            city: req.headers['x-vercel-ip-city']
         });
     }
 };
