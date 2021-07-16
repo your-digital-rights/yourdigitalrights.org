081c7a3d8d7f326a4fc7987317a0ddbea17e82cc
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2021-07-16 11:12:28 +0200

    Change geolocation headers to lower case

--- src/pages/api/geolocation.js
+++ src/pages/api/geolocation.js
@@ -1,11 +1,11 @@
 export default async (req, res) => {
-    if (req.headers['X-Vercel-IP-Country'] === undefined) {
+    if (req.headers['x-vercel-ip-country'] === undefined) {
         res.status(400).json({err: "Geolocation unavaialble."});
     } else {
         res.status(200).json({ 
-            country: req.headers('X-Vercel-IP-Country'),
-            region: req.headers('X-Vercel-IP-Country-Region'),
-            city: req.headers('X-Vercel-IP-City')
+            country: req.headers('x-vercel-ip-country'),
+            region: req.headers('x-vercel-ip-country-region'),
+            city: req.headers('x-vercel-ip-city')
         });
     }
 };
