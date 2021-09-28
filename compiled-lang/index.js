6fafde637b914593cf9b90d4348a789706179065
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2021-09-28 15:40:31 +0200

    Add AZ local to compiled lang index

--- compiled-lang/index.js
+++ compiled-lang/index.js
@@ -2,5 +2,6 @@ import * as en from './en';
 import * as it from './it';
 import * as es from './es';
 import * as pt from './pt';
+import * as az from './az';
 
-export {en, it, es, pt};
+export {en, it, es, pt, az};
