54b04f7c0fa3cdc5d85b2a577e57e15226bd590e
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2021-10-13 11:12:09 +0200

    Fix translation error

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,7 +1,7 @@
-import { createTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 // Create a theme instance.
-const theme = createTheme({
+const theme = createMuiTheme({
   overrides: {
     MuiCssBaseline: {
       '@global': {
