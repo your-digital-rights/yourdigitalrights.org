5589e6a8e02c094ebbe71843e5dde64473cbbd44
Hans Sprecher <hans.sprecher@gmail.com>
2021-10-13 01:19:54 -0400

    feat: new api to update status and redirect

---
+++ src/pages/api/status.js
@@ -0,0 +1,47 @@
+import aws from "aws-sdk";
+
+aws.config.update({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: process.env.REGION,
+  endpoint: process.env.ENDPOINT,
+});
+
+const dynamodb = new aws.DynamoDB();
+
+export default async (req, res) => {
+  const { status, uuid } = req.query;
+
+  if (["SUCCESS", "PARTIAL", "DECLINED", "NO_REPLY"].indexOf(status) === -1) {
+    res.statusCode = 400;
+    res.send();
+    return;
+  }
+
+  dynamodb.updateItem({
+    ExpressionAttributeNames: {
+      "#S": "status",
+    },
+    ExpressionAttributeValues: {
+      ":s": {
+        S: status,
+      },
+    },
+    Key: {
+      "id": {
+        S: uuid,
+      },
+    },
+    ReturnValues: "ALL_NEW",
+    TableName: 'YDRFollowUps',
+    UpdateExpression: "SET #S = :s",
+  }, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.send();
+    } else {
+      res.redirect(302, `/r/${uuid}`);
+    }
+    return;
+  });
+};
