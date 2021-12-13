077bbeb83e3b1eeabd75fa32d4279703fb5d7c5c
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2021-12-13 22:25:28 +0100

    Logo

--- src/components/OrgHero/index.js
+++ src/components/OrgHero/index.js
@@ -18,7 +18,7 @@ const Hero = ({ classes, selectedCompany }) => {
           <div id="org-logo" className={classes.orgLogo}>
             <img
               className={classes.logo}
-              src={`//logo.uplead.com/${selectedCompany.url}?size=24`}
+              src={`//logo.uplead.com/${selectedCompany.url}`}
             />
           </div>
         )}
