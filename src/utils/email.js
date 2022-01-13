19b9afd118bbe03dcab4dbed8b37c37e70593ea2
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2022-01-13 15:16:31 +0100

    refactor

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,7 +1,4 @@
 export default function getInboundEmailAddress(uuid, action) {
-  if (process.env.VERCEL_ENV === 'production') {
-    return `${uuid}.${action}@inbound.yourdigitalrights.org`;
-  } else {
-    return `${uuid}.${action}@inbound-dev.yourdigitalrights.org`;
-  }
+  const part = process.env.VERCEL_ENV === 'production'? 'inbound' : 'inbound-dev';
+  return `${uuid}.${action}@${part}.yourdigitalrights.org`;
 };
