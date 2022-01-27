1bfb185793192a196b8a27d0f09d301361ba85e2
Rafa Prada <mprada.rafael@gmail.com>
2022-01-27 10:36:26 +0100

    Kaspersky logo addition

--- src/components/PressCoverage/index.js
+++ src/components/PressCoverage/index.js
@@ -10,6 +10,7 @@ const items = [
   { name: "Fast Company", image: "/images/press/fastcompany.png", url: "https://www.fastcompany.com/90310803/here-are-the-data-brokers-quietly-buying-and-selling-your-personal-information" },
   { name: "Business Insider", image: "/images/press/business-insider.png", url: "https://www.businessinsider.com/how-to-delete-yourself-from-the-internet?IR=T" },
   { name: "Wired", image: "/images/press/wired.png", url: "https://www.wired.com/story/delete-yourself-from-internet/" },
+  { name: "Karspesky", image: "/images/press/kaspersky_b.png", url: "https://www.kaspersky.com/resource-center/preemptive-safety/how-to-stop-data-brokers-from-selling-your-personal-information" },
   { name: "How To Geek", image: "/images/press/howtogeek.png", url: "https://www.howtogeek.com/570124/how-to-delete-your-personal-information-from-people-finder-sites/" },
   //{ name: "MSN", image: "/images/press/msn.png", url: "https://www.msn.com/en-us/news/technology/how-to-delete-yourself-from-the-internet-with-5-different-methods/ar-BB1aZJ4A?li=BBnb7Kz" },
   { name: "Indivigital", image: "/images/press/indivigital.png", url: "https://indivigital.com/news/new-gdpr-tool-targets-google-facebook-with-automated-right-to-be-forgotten-requests/" },
