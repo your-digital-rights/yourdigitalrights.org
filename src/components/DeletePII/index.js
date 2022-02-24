e4be4d879d209a6d69714c620c9eef32996888cd
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2022-02-24 12:02:11 +0100

    Messgae on data deletion success

--- src/components/DeletePII/index.js
+++ src/components/DeletePII/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Typography from "@material-ui/core/Typography";
 import styles from "./styles";
 import { withStyles } from "@material-ui/core/styles";
@@ -10,6 +11,7 @@ import { useRouter } from 'next/router'
 const DeletePII = ({ classes, uuid }) => {
   const router = useRouter();
   const ref = uuid ? uuid.split("-")[0] : "";
+  const [dataDeleted, updateDataDeleted] = React.useState();
   
   const trackDeletePII = (type) => {
     tracking.trackDeletePII(type);
@@ -33,7 +35,7 @@ const DeletePII = ({ classes, uuid }) => {
     );
     
     trackDeletePII("Delete " + type + " PII");
-    router.push('/'); // change to thank you page
+    updateDataDeleted(type);
   };
 
   return (
@@ -47,35 +49,55 @@ const DeletePII = ({ classes, uuid }) => {
         >
           <FormattedMessage id="deletePII.heading" defaultMessage="Delete my data" />
         </Typography>
+        { !dataDeleted && 
+          <>
+            <Typography color="inherit" className={classes.intro}>
+              <FormattedMessage
+                id="deletePII.intro"
+                defaultMessage="Your personal data will be automatically deleted 120 days after a request was made, however you can choose to delete your data now. Do you wish to delete the data which is associated with this request (ref: {ref}), or with all of your requests?"
+                values={{ 
+                  ref: <a target="_blank" href={`/r/${uuid}`}>{ref}</a>,
+                }}
+              />
+            </Typography>          
+            <div className={classes.buttons}>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                className={classes.leftButton}
+                onClick={() => onClick('request')}
+              >
+                <FormattedMessage id="deletePII.deleteSpecificPII" defaultMessage="This request" />
+              </Button>                          
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                className={classes.rightButton}
+                onClick={() => onClick('all')}
+              >
+                <FormattedMessage id="deletePII.deleteAllPII" defaultMessage="All requests" />
+              </Button>
+          </div>
+        </>
+      }
+      { dataDeleted == 'all' &&
         <Typography color="inherit" className={classes.intro}>
           <FormattedMessage
-            id="deletePII.intro"
-            defaultMessage="Your personal data will be automatically deleted 120 days after a request was made, however you can choose to delete your data now. Do you wish to delete the data which is associated with this request (ref: {ref}), or with all of your requests?"
-            values={{ 
-              ref: <a target="_blank" href={`/r/${uuid}`}>{ref}</a>,
-            }}
+            id="deletePII.doneAll"
+            defaultMessage="We have successfully deleted the personal data associated with all of your requests."
           />
-        </Typography>          
-        <div className={classes.buttons}>
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            className={classes.leftButton}
-            onClick={() => onClick('request')}
-          >
-            <FormattedMessage id="deletePII.deleteSpecificPII" defaultMessage="This request" />
-          </Button>                          
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            className={classes.rightButton}
-            onClick={() => onClick('all')}
-          >
-            <FormattedMessage id="deletePII.deleteAllPII" defaultMessage="All requests" />
-          </Button>
-        </div>
+        </Typography>       
+      }
+      { dataDeleted == 'request' &&
+        <Typography color="inherit" className={classes.intro}>
+          <FormattedMessage
+            id="deletePII.doneRequest"
+            defaultMessage="We have successfully deleted the personal data associated with this request."
+          />
+        </Typography>       
+      }      
       </div>
     </div>
   );
