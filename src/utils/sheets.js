26537a633ea5d28d68fe8436012ef2042dc0c005
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2022-03-01 00:16:51 +0100

    Another fetch fix

--- src/utils/sheets.js
+++ src/utils/sheets.js
@@ -1,16 +1,17 @@
-import { DOMAIN_URL } from "./domain";
+import { DOMAIN } from "./domain";
 import fetch from "isomorphic-fetch";
 var data = null;
 
 export default async function fetchData() {
+  const url = process.env.VERCEL_URL ? `//${process.env.VERCEL_URL}/api/companies` : 'http://localhost:3000/api/companies';
   if (data == null) {
-    data = fetch(`${DOMAIN_URL}/api/companies`)
+    data = fetch(url)
       .then((response) => {
         if (response.ok) {
           return response.json();
         }
 
-        throw new Error(`HTTP error ${response.status} from '/api/companies'`);
+        throw new Error(`HTTP error ${response.status} from ${url}`);
       })
       .then((json) => {
         return json;
