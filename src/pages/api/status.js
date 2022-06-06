9cb7cdec0de6001b1e441e1b77335d21818a7ea6
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2022-06-07 00:13:27 +0200

    statusHistory working

--- src/pages/api/status.js
+++ src/pages/api/status.js
@@ -20,88 +20,89 @@ const dynamodb = new aws.DynamoDB();
  */
 export default async (req, res) => {
   return new Promise((resolve, reject) => {
-  const { status, uuid, url } = req.query;
+    const { status, uuid, url } = req.query;
 
-  res.setHeader('Content-Type', 'application/json');
-  if (!uuid || !status) {
-    res.statusCode = 400;
-    res.send({
-      error: 'Missing uuid or status',
-    });
-    return;
-  }
-
-  if (["SUCCESS", "PARTIAL", "DECLINED", "NO_REPLY"].indexOf(status) === -1) {
-    res.statusCode = 400;
-    res.send();
-    return;
-  }
-
-  dynamodb.updateItem({
-    TableName: 'YDRFollowups',    
-    Key: {
-      "id": {
-        S: uuid,
-      },
-    },
-    UpdateExpression: "SET #S = :s",
-    ExpressionAttributeNames: {
-      "#S": "status",
-    },
-    ExpressionAttributeValues: {
-      ":s": {
-        S: status,
-      },
-    },
-    ReturnValues: "ALL_NEW",
-  }, (err, data) => {
-    if (err) {
-      res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    if (!uuid || !status) {
+      res.statusCode = 400;
       res.send({
-        error: 'Could not update status in YDRFollowups.',
+        error: 'Missing uuid or status',
       });
-    } else {  
-      dynamodb.updateItem({
-        TableName: 'YDRRequests',    
-        Key: {
-          "id": {
-            S: uuid,
-          },
-        },
-        UpdateExpression: "SET statusHistory = list_append(statusHistory, :s)",
-        ExpressionAttributeValues: {
-          ":s": {
-            M: { 
-              "Date": {
-                S: new DateTime().toUTC().toISO(),
-              },
-              "Status": {
-                S: status,
-              }
-            }  
-          },
-        },
-        ReturnValues: "ALL_NEW",
-      }, (err, data) => {
-        if (err) {
-          res.statusCode = 500;
-          res.send({
-            error: 'Could not update status in YDRRequests.',
-          });
-        } else { 
-          if (url) {
-            res.redirect(302, `/r/${uuid}`);
-          } else {
-            res.statusCode = 200;
-            res.send({
-              success: 'Saved status.',
-            });
-          }
-        }
-      });
+      return;
     }
-    resolve();
-    return;
+
+    if (["SUCCESS", "PARTIAL", "DECLINED", "NO_REPLY"].indexOf(status) === -1) {
+      res.statusCode = 400;
+      res.send();
+      return;
+    }
+
+    dynamodb.updateItem({
+      TableName: 'YDRFollowups',    
+      Key: {
+        "id": {
+          S: uuid,
+        },
+      },
+      UpdateExpression: "SET #S = :s",
+      ExpressionAttributeNames: {
+        "#S": "status",
+      },
+      ExpressionAttributeValues: {
+        ":s": {
+          S: status,
+        },
+      },
+      ReturnValues: "ALL_NEW",
+    }, (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.send({
+          error: 'Could not update status in YDRFollowups: ' + err,
+        });
+      } else {  
+        dynamodb.updateItem({
+          TableName: 'YDRRequests',    
+          Key: {
+            "id": {
+              S: uuid,
+            },
+          },
+          UpdateExpression: "SET statusHistory = list_append(if_not_exists(statusHistory, :empty_list), :s)",
+          ExpressionAttributeValues: {
+            ":s":{L: [{
+              M: { 
+                "Date": {
+                  S: DateTime.now().toUTC().toISO(),
+                },
+                "Status": {
+                  S: status,
+                }
+              }  
+            }]},
+            ":empty_list": {L: [] },
+          },
+          ReturnValues: "ALL_NEW",
+        }, (err, data) => {
+          if (err) {
+            res.statusCode = 500;
+            res.send({
+              error: 'Could not update status in YDRRequests: ' + err,
+            });
+          } else { 
+            if (url) {
+              res.redirect(302, `/r/${uuid}`);
+            } else {
+              res.statusCode = 200;
+              res.send({
+                success: 'Saved status.',
+              });
+            }
+          }
+        });
+      }
+      resolve();
+      return;
+    });
   });
-});
 };
