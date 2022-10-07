55db6653e4d5f1da463606a7c80cb6a19afcce08
Yoav Aviram <yoav.aviram@gmai.com>
2022-10-07 15:11:42 +0200

    Privacy alerts page done

--- src/pages/privacy-alerts.js
+++ src/pages/privacy-alerts.js
@@ -18,8 +18,9 @@ const styles = (theme) => ({
     marginTop: "60px",
   },
   inner: {
+    borderRadius: "20px",
     color: "black",
-    backgroundColor: "#039277",
+    backgroundColor: "white",
     paddingLeft: 120,
     paddingRight: 120,
     paddingTop: 50,
@@ -30,17 +31,12 @@ const styles = (theme) => ({
     },
   },
   substack: {
+    marginTop: "20px",
     display: "flex",
     flexDirection: "row ",
     alignItems: "center",
     justifyContent: "center",
   },  
-  title: {
-    color: "white",
-  },
-  strapline: {
-    color: "white",
-  }  
 });
 
 
@@ -63,10 +59,10 @@ const PrivacyAlerts = ({ classes, router }) => {
       <Nav />
       <div className={classes.container}>
         <Paper className={classes.inner} elevation={2} >
-          <Typography component="h1" variant="h4" gutterBottom={true} className={classes.title} >
+          <Typography component="h1" variant="h4" gutterBottom={true} >
             <FormattedMessage id="privacyAlerts.title" defaultMessage="Keep it private!" />
           </Typography>
-          <Typography component="p" variant="h5" gutterBottom={true} className={classes.strapline}>
+          <Typography component="p" variant="h5" gutterBottom={true} >
             <FormattedMessage
               id="privacyAlerts.strapline"
               defaultMessage="Your data is all over the internet, but you don't know how to get it back? We can help you get your privacy to a better place."
@@ -75,50 +71,17 @@ const PrivacyAlerts = ({ classes, router }) => {
           <br/>          
           <Typography gutterBottom={true}>
             <FormattedMessage id="privacyAlerts.description1" defaultMessage="Improve your privacy posture over time by spending only 5 minutes a month. Sign up to our privacy alerts and each month we will email you a list of 3 companies with the worst privacy track record for the month. All you need to do is click the links in the email to opt-out." />
-          </Typography>
-            <div className={classes.substack}>
-              <iframe src="https://consciousdigital.substack.com/embed" width="450" height="100"  frameBorder="0" scrolling="no" ></iframe>
-            </div>     
-          <Typography gutterBottom={true}>
-            <FormattedMessage id="privacyAlerts.description2" defaultMessage="Our research team identifies companies while taking into consideration:" />
-          </Typography>
-          <ul className={classes.list}>
-            <li>
-              <FormattedMessage
-                id="privacyAlerts.dscriptionBullit1"
-                defaultMessage="Companies with the most opt-out requests for the month"
-              />
-            </li>
-            <li>
-              <FormattedMessage
-                id="privacyAlerts.dscriptionBullit2"
-                defaultMessage="New data brokers (companies selling personal information including location, and facial recognition data)"
-              />
-            </li>
-            <li>
-              <FormattedMessage
-                id="privacyAlerts.dscriptionBullit3"
-                defaultMessage="Companies who have experienced a recent data breach"
-              />
-            </li>
-            <li>
-              <FormattedMessage
-                id="privacyAlerts.dscriptionBullit4"
-                defaultMessage="Companies who have mishandled personal information "
-              />
-            </li>
-          </ul>     
-          <Typography color="inherit" className={classes.intro} gutterBottom={true}>
-            <FormattedMessage
-              id="privacyAlerts.description3"
-              defaultMessage="Each email also includes new privacy best practices we have identified, which you can implement straight away. This is a paid service which helps ensure our long-term financial stability and independence."
-            />
-            <br/><br/>
+          </Typography>    
+          <br/>
+          <Typography color="inherit" className={classes.intro} gutterBottom={true}>            
             <FormattedMessage
               id="privacyAlerts.description4"
               defaultMessage="This is a paid service which helps ensure our long-term financial stability and independence."
             />
           </Typography>            
+          <div className={classes.substack}>
+              <iframe src="https://consciousdigital.substack.com/embed" width="450" height="100"  frameBorder="0" scrolling="no" ></iframe>
+            </div>               
         </Paper>
       </div>
       <Footer showRoadmap={false} />
