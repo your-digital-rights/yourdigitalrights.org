9de10da9623afcedaf4ef443f82a65b7f141be72
Yoav Aviram <yoav.aviram@gmai.com>
2022-11-18 16:15:54 +0100

    update geolocaiton api to support Canada and Viriginia

--- src/utils/geolocation.js
+++ src/utils/geolocation.js
@@ -14,11 +14,17 @@ async function getRegulationbyGeolocation() {
       })
       .then((resultJson) => {
         if (resultJson['country'] === 'US') {
-            return 'CCPA';
+            if (resultJson['region'] === 'VA') {
+              return 'VCDPA';
+            } else {
+              return 'CCPA';
+            }
           } else if (resultJson['country'] === 'GB') {
             return 'GDPRUK';
           } else if (resultJson['country'] === 'BR') {
-            return 'LGPD';            
+            return 'LGPD';     
+          } else if (resultJson['country'] === 'CA') {
+            return 'PIPEDA';                        
           } else {
             return 'GDPR';
           }
