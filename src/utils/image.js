7cbfd98d6e8ebeadcdbdb9f9ec1289af5414cff1
Yoav Aviram <yoav.aviram@gmai.com>
2022-12-01 18:48:19 +0100

    add image.js

---
+++ src/utils/image.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+
+const ImageWithFallback = (props) => {
+    const { src, fallbackSrc, ...rest } = props;
+    const [imgSrc, setImgSrc] = useState(src);
+
+    return (
+        <Image
+            {...rest}
+            src={imgSrc}
+            onError={() => {
+                setImgSrc(fallbackSrc);
+            }}
+        />
+    );
+};
+
+export default ImageWithFallback;
