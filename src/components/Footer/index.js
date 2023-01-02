a356f4b69cf758c5fc1a350c2610018214726218
Yoav Aviram <yoav.aviram@gmai.com>
2023-01-02 09:58:28 +0100

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -164,9 +164,6 @@ const Footer = ({ classes, showRoadmap=true }) => {
           <Typography gutterBottom={true} color="textSecondary" variant="subtitle2">
             {DisclaimerText}
           </Typography>
-          <Typography gutterBottom={true} color="textSecondary" variant="subtitle2">
-            This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.
-          </Typography>
         </div>
       </div>
     </div>
