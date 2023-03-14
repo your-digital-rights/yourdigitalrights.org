edd0b3d858cee087c1ef2fb92b27847c08c13a27
Yoav Aviram <yoav.aviram@gmai.com>
2023-03-14 14:25:20 +0100

    final

--- src/components/AboutOrg/index.js
+++ src/components/AboutOrg/index.js
@@ -2,16 +2,15 @@ import Typography from "@material-ui/core/Typography";
 import styles from "./styles";
 import { withStyles } from '@material-ui/core/styles';
 import { FormattedMessage } from "react-intl";
+import { useState } from "react";
 
 
 function Capitalize(str){
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
-
 const AboutOrg = ({ classes, selectedCompany }) => {
-  const description = selectedCompany.description.length > 0 ? selectedCompany.description : selectedCompany.slogan;
-  console.log(description);
+  const [showMore, setShowMore] = useState(false);  
   return (
     <div className={classes.about}>
       <div id="about-org" className={classes.container}>
@@ -33,16 +32,73 @@ const AboutOrg = ({ classes, selectedCompany }) => {
             </Typography>
           </div>
           <div id="about-detail-text" className={classes.detailText}>
-            { description.length > 0 && (
-              <>
+            { (selectedCompany.slogan.length > 0 || (selectedCompany.description.length > 0)) && (
               <strong>
                 <FormattedMessage id="aboutOrg.description" defaultMessage="In their own words:" />
+                <br/>
               </strong>
-              <br/>
-              {description}
-              <br/>
+            )}
+            { selectedCompany.slogan.length > 0 && (
+              <>
+                {selectedCompany.slogan}
+                <br/>
               </>
             )}
+            { selectedCompany.description.length > 0 && (
+              <>
+                {showMore ? selectedCompany.description.substring(0, 2000) : `${selectedCompany.description.substring(0, 300)}`}
+                <br/>
+                <a className={classes.showMore} onClick={() => setShowMore(!showMore)}>
+                  {showMore ? "Show less" : "Show more"}
+                </a>
+                <br/>
+              </>
+            )}
+            { selectedCompany.industries.length > 0 && (
+              <>
+                <strong>
+                  <FormattedMessage id="aboutOrg.industry" defaultMessage="Industry:" />{" "}
+                </strong>
+                {selectedCompany.industries}
+                <br/>
+              </>
+            )}  
+            { selectedCompany.specialties.length > 0 && (
+              <>
+                <strong>
+                  <FormattedMessage id="aboutOrg.specialties" defaultMessage="Company Specialties:" />{" "}
+                </strong>
+                {selectedCompany.specialties}
+                <br/>
+              </>
+            )}
+            { selectedCompany.type.length > 0 && (
+              <>
+                <strong>
+                  <FormattedMessage id="aboutOrg.type" defaultMessage="Company Type:" />{" "}
+                </strong>
+                {selectedCompany.type}
+                <br/>
+              </>
+            )}     
+            { selectedCompany.headquarters.length > 0 && (
+              <>
+                <strong>
+                  <FormattedMessage id="aboutOrg.headquarters" defaultMessage="Headquarters:" />{" "}
+                </strong>
+                {selectedCompany.headquarters}
+                <br/>
+              </>
+            )}    
+            { selectedCompany.founded.length > 0 && (
+              <>
+                <strong>
+                  <FormattedMessage id="aboutOrg.founded" defaultMessage="Founded:" />{" "}
+                </strong>
+                {selectedCompany.founded}
+                <br/>
+              </>
+            )}                                                               
             <strong>
               <FormattedMessage id="aboutOrg.email" defaultMessage="Email:" />{" "}
             </strong>
@@ -55,21 +111,18 @@ const AboutOrg = ({ classes, selectedCompany }) => {
               {selectedCompany.email}
             </a>
             <br />
-            {selectedCompany.privacyPolicy && (
-              <>
-                <strong>
-                  <FormattedMessage id="aboutOrg.privacyPolicy" defaultMessage="Privacy Policy:" />{" "}
-                </strong>
-                <a
-                  target="_blank"
-                  rel="noreferrer noopener"
-                  href={`${selectedCompany.privacyPolicy}`}
-                  className={classes.link}
-                >
-                  {selectedCompany.privacyPolicy}
-                </a>
-              </>
-            )}
+            {selectedCompany.privacyPolicy.length > 0 && (
+              <strong>
+                <FormattedMessage 
+                  id="aboutOrg.privacyPolicy" 
+                  defaultMessage="View {org}'s <a>Privacy Policy</a>"
+                  values={{
+                    org: selectedCompany.name,
+                    a: txt => (<a target="_blank" rel="noreferrer noopener"href={selectedCompany.privacyPolicy} className={classes.link} ><strong>{txt}</strong></a>),
+                  }}  
+                />
+              </strong>
+            )}            
           </div>
         </div>
       </div>
