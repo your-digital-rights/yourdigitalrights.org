699753cb104d9e08e35c12e4a09c12966967a7e4
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2023-08-29 12:31:54 +0200

    Better error on api fetch fail

--- src/utils/domains.js
+++ src/utils/domains.js
@@ -31,7 +31,7 @@ async function fetchDomainDetails(domain) {
         if (response.status <= 400) {
           return response.json();
         }
-        throw new Error(`HTTP error ${response.status} from ${url}`);
+        throw new Error(`HTTP error ${response.status} from '${url}', headers: ${request.headers}`);
       })
       .then((json) => {
         return json;
