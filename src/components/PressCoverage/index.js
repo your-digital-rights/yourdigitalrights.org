cd29c6b9327e099d990c561bf053bcf904c715d1
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2024-02-07 16:26:23 +0100

    Add press

--- src/components/PressCoverage/index.js
+++ src/components/PressCoverage/index.js
@@ -12,11 +12,13 @@ const items = [
   { name: "Fast Company", image: "/images/press/fastcompany.png", url: "https://www.fastcompany.com/90310803/here-are-the-data-brokers-quietly-buying-and-selling-your-personal-information" },
   { name: "Business Insider", image: "/images/press/business-insider.png", url: "https://www.businessinsider.com/how-to-delete-yourself-from-the-internet?IR=T" },
   { name: "Wired", image: "/images/press/wired.png", url: "https://www.wired.com/story/delete-yourself-from-internet/" },
-  { name: "Karspesky", image: "/images/press/kaspersky_b.png", url: "https://www.kaspersky.com/resource-center/preemptive-safety/how-to-stop-data-brokers-from-selling-your-personal-information" },
+  //{ name: "Karspesky", image: "/images/press/kaspersky_b.png", url: "https://www.kaspersky.com/resource-center/preemptive-safety/how-to-stop-data-brokers-from-selling-your-personal-information" },
   { name: "How To Geek", image: "/images/press/howtogeek.png", url: "https://www.howtogeek.com/570124/how-to-delete-your-personal-information-from-people-finder-sites/" },
   //{ name: "MSN", image: "/images/press/msn.png", url: "https://www.msn.com/en-us/news/technology/how-to-delete-yourself-from-the-internet-with-5-different-methods/ar-BB1aZJ4A?li=BBnb7Kz" },
+  { name: "The Sun", image: "/images/press/the-sun.png", url: "https://www.the-sun.com/tech/7794302/billions-facebook-photos-scraped-ai-police-lineup/" },
+  { name: "Vanity Fair", image: "/images/press/vanity-fair.png", url: "https://www.vanityfair.fr/article/6-facons-de-disparaitre-internet-pour-toujours" },
   { name: "Bed Credit", image: "/images/press/bc-logo-2x.png", url: "https://www.badcredit.org/news/yourdigitalrights-offers-digital-privacy-resources/" },
-  { name: "Indivigital", image: "/images/press/indivigital.png", url: "https://indivigital.com/news/new-gdpr-tool-targets-google-facebook-with-automated-right-to-be-forgotten-requests/" },
+  
 ];
 
 
