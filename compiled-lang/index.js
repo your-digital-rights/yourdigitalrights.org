6c624c74ac0fccf7e70582370c5ec37cea8d6413
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2025-02-28 16:03:23 +0100

    translations

--- compiled-lang/index.js
+++ compiled-lang/index.js
@@ -6,6 +6,6 @@ import * as es from './es';
 import * as pt from './pt';
 import * as az from './az';
 import * as ja from './ja';
-import * as cn from './cn';
+import * as cn from './zh';
 
-export {en, it, es, pt, de, fr, az, ja, cn};
+export {en, it, es, pt, de, fr, az, ja, zh};
