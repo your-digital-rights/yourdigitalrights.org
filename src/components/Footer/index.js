aec6e5876e8e10258f875f31067eda5fbe930352
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2025-05-09 11:38:18 +0200

    faq v7

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,79 +1,14 @@
 import Button from "@mui/material/Button";
 import { FormattedMessage } from "react-intl";
 import Typography from "@mui/material/Typography";
-import { container } from "../../styles/layout";
-import withStyles from '@mui/styles/withStyles';
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { useTheme } from '@mui/material/styles';
 import tracking from "../../utils/tracking";
 import Link from 'next/link';
 import Image from 'next/image';
 import Vercel from "../../../public/images/powered-by-vercel.svg";
-
-const styles = (theme) => ({
-  root: {
-    ...container,
-  },
-  inner: {
-    // borderTop: "2px solid #005ea5",
-    marginTop: 30,
-    paddingTop: 30,
-    paddingBottom: 30,
-    [theme.breakpoints.up("md")]: {
-      paddingRight: 30,
-      paddingLeft: 30,
-    },
-  },
-  innerLeft: {
-    // borderTop: "2px solid #005ea5",
-    width: "20%",
-    float: "left",
-    marginBottom: 30,
-    [theme.breakpoints.down('md')]: {
-      width: "100%",
-      textAlign: "center",
-    },
-  },
-  innerRight: {
-    // borderTop: "2px solid #005ea5",
-    width: "75%",
-    float: "right",
-    marginBottom: 60,
-    fontSize: '14px',
-    fontWeight: "400",
-    [theme.breakpoints.down('md')]: {
-      width: "100%",
-      fontSize: '10px',
-    },
-  },
-  Copyright: {
-    textAlign: "center",
-  },
-  WishList: {
-    textAlign: "center",
-  },
-  wishButton: {
-    marginTop: "-25px",
-    borderRadius: "24px 24px 24px 24px",
-    color: "white",
-    fontWeight: "600",
-    padding: "10px 20px",
-    "&:hover": {
-      background: "#cf8600",
-    },
-  },
-  DisclaimerLink: {
-    color: "#005ea5",
-    fontWeight: "600",
-    textDecoration: "none",
-    "&:hover": {
-      textDecoration: "underline",
-    },
-  },
-  VercelLogo: {
-    height: "1.8em",
-    width: "auto",
-    marginTop: "5px",   
-  }
-});
+import { StyledLink, StyledButton, StyledVercelLogo } from './styles';
 
 const WishButtonText = (
   <FormattedMessage id="footer.wishList" defaultMessage="make a wish on our roadmap" />
@@ -100,78 +35,106 @@ const QueryText = (
   />
 );
 
-const Footer = ({ classes, showRoadmap=true }) => {
+const Footer = ({ showRoadmap = true }) => {
+  const theme = useTheme();
   const trackWishlist = () => {
     tracking.trackWishlist();
   };
 
   return (
-    <div className={classes.root}>
+    <Box sx={{ maxWidth: "1000px", margin: "0 auto", padding: "0 30px" }}>
       {showRoadmap && (
-        <div className={classes.WishList}>
-          <Button
-            onClick={() => trackWishlist()}
+        <Box sx={{ textAlign: "center", mb: 4 }}>
+          <StyledButton
+            onClick={trackWishlist}
             variant="contained"
             href="https://wishlist.yourdigitalrights.org/"
             color="secondary"
             type="submit"
-            className={classes.wishButton}
+            target="_blank"
           >
             {WishButtonText}
-          </Button>
-        </div>
+          </StyledButton>
+        </Box>
       )}
-      <div className={classes.inner}>
-        <div className={classes.innerLeft}>
+
+      <Box sx={{ 
+        mt: 4, 
+        pt: 4, 
+        pb: 4,
+        borderTop: "1px solid #eaeaea",
+        display: "flex",
+        flexDirection: { xs: "column", md: "row" },
+        gap: 4
+      }}>
+        <Box sx={{ 
+          width: { xs: "100%", md: "20%" },
+          textAlign: { xs: "center", md: "left" }
+        }}>
           <Typography component="p" variant="subtitle2">
             <FormattedMessage
               id="footer.madeBy"
               defaultMessage="<a>Made by Conscious Digital</a>"
               values={{
-                a: txt=> (<a target="_blank" href="https://consciousdigital.org" className={classes.DisclaimerLink}>{txt}</a>)
+                a: txt => (<StyledLink target="_blank" href="https://consciousdigital.org">{txt}</StyledLink>)
               }}
             />
-          </Typography>   
+          </Typography>
           <Typography component="p" variant="subtitle2">
             <FormattedMessage
               id="footer.stats"
               defaultMessage="<a>Statistics</a>"
               values={{
-                a: txt=> (<Link href="/stats" className={classes.DisclaimerLink}>{txt}</Link>)
+                a: txt => (
+                  <Link href="/stats" passHref legacyBehavior>
+                    <a style={{ color: "#005ea5", fontWeight: 600, textDecoration: "none" }}>
+                      {txt}
+                    </a>
+                  </Link>
+                )
               }}
             />
-          </Typography>                          
+          </Typography>
           <Typography component="p" variant="subtitle2">
             <FormattedMessage
               id="footer.privacyPolicy"
               defaultMessage="<a>Privacy Policy</a>"
               values={{
-                a: txt=> (<Link href="/privacy" className={classes.DisclaimerLink}>{txt}</Link>)
+                a: txt => (
+                  <Link href="/privacy" passHref legacyBehavior>
+                    <a style={{ color: "#005ea5", fontWeight: 600, textDecoration: "none" }}>
+                      {txt}
+                    </a>
+                  </Link>
+                )
               }}
             />
           </Typography>
-
           <Typography component="p" variant="subtitle2">
-            <a className={classes.DisclaimerLink} target="_blank" rel="nofollow" href="https://www.uplead.com">Logos by UpLead</a>
-          </Typography> 
-          <a            
-            href="https://vercel.com?utm_source=Conscious%20Digital&utm_campaign=oss"
-          >
-            <Image 
+            <StyledLink target="_blank" rel="nofollow" href="https://www.uplead.com">
+              Logos by UpLead
+            </StyledLink>
+          </Typography>
+          <Link target="_blank" href="https://vercel.com?utm_source=Conscious%20Digital&utm_campaign=oss" passHref>
+            <StyledVercelLogo 
               src={Vercel}
               alt={PoweredBy}
-              className={classes.VercelLogo}
             />
-          </a>
-        </div>
-        <div className={classes.innerRight}>
-          <Typography gutterBottom={true} color="textSecondary" variant="subtitle2">
+          </Link>
+        </Box>
+
+        <Box sx={{ 
+          width: { xs: "100%", md: "80%" },
+          fontSize: { xs: '10px', md: '14px' },
+          fontWeight: 400
+        }}>
+          <Typography gutterBottom color="textSecondary" variant="subtitle2">
             {DisclaimerText}
           </Typography>
-        </div>
-      </div>
-    </div>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
-export default withStyles(styles)(Footer);
+export default Footer;
